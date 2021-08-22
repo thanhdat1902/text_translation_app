aea15ed3d7790970196eebf01362d6b5da0110f8
nctai19125120 <63503421+nctai19125120@users.noreply.github.com>
2021-08-22 18:57:05 +0700

    Edit LoginActivity and SliderActivity

---
+++ app/src/main/java/com/cs426/imageetranslation/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.cs426.imageetranslation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activitity_slider_intro);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnLogin: {
+                startActivity(new Intent(this, SliderActivity.class));
+                break;
+            }
+        }
+    }
+}
